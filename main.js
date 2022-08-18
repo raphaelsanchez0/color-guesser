6c5f748f3464527c45c8884f15b7e9cc865e1f44
raphaelsanchez0 <sanchezraphael0@gmail.com>
2022-08-18 16:19:30 -0400

    Added interactivity to buttons and made them semi-functional

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let colorGrid = document.querySelectorAll(".color-grid-block");
-let resetBtn = document.querySelector("#reset-btn")
+const colorGrid = document.querySelectorAll(".color-grid-block");
+const resetBtn = document.querySelector("#reset-btn");
+const rgbTextValue = document.querySelector('#rgb-value')
 
 function generateRandomRGBvalue(){
     const max = 256;
@@ -8,16 +9,37 @@ function generateRandomRGBvalue(){
     //removed decimal
     return Math.floor(rand);
 }
+/*generates a number between 0-8 in order to choose
+which box is the right color*/
+function generateCorrectColorIndex(){
+    return Math.floor(Math.random()*colorGrid.length)
+
+}
 
 function resetColors (){
     for(let i = 0;i<colorGrid.length;i++){
         colorGrid[i].style.backgroundColor = `rgb(${generateRandomRGBvalue()},${generateRandomRGBvalue()},${generateRandomRGBvalue()})`;
-
+        
+        
+        //rgbTextValue.innerHTML = `rgb${colorGrid[generateRandomRGBvalue]}`
     }
+    const correntColorIndex = generateCorrectColorIndex();
+    const correntRGBvalue = colorGrid[correntColorIndex].style.backgroundColor;
+    console.log(correntColorIndex+1);
+    rgbTextValue.innerHTML = correntRGBvalue;
 }
 
-resetBtn.addEventListener("click", function(e){
+resetBtn.addEventListener("click", event => {
     resetColors();
 });
+
+colorGrid.forEach(block =>{
+    block.addEventListener('click', event =>{
+        console.log(colorGrid.length);
+        // if(colorGrid.indexOf(block) = correntColorIndex ){
+        //     console.log('thats the right block!')
+        // }
+    })
+})
 
 resetColors();
